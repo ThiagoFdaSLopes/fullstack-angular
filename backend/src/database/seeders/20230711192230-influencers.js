eb5abdedde72a8fa9e7d5fa71eecaf607cebd963
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 17:58:42 -0300

    fix: Tipo de followers

--- backend/src/database/seeders/20230711192230-influencers.js
+++ backend/src/database/seeders/20230711192230-influencers.js
@@ -6,28 +6,32 @@ module.exports = {
         name: 'Billie Eilish',
         platform: 'Instagram',
         country: 'EUA',
-        followers: 109000000,
+        followers: '109000000',
+        category: "Artista"
       },
       {
         image: 'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2023-06/230629-travis-scott-mjf-1458-7f6898.jpg',
         name: 'Travis Scott',
         platform: 'Instagram',
         country: 'EUA',
-        followers: 49800000,
+        followers: '49800000',
+        category: "Artista"
       },
       {
         image: 'https://pyxis.nymag.com/v1/imgs/181/5b4/959542d212df52641126e1605d1297f927-20-chester-bennington.jpg',
         name: 'Chester Bennington',
         platform: 'Instagram',
         country: 'EUA',
-        followers: 1400000,
+        followers: '1400000',
+        category: "Artista"
       },
       {
         image: 'https://conteudo.imguol.com.br/c/esporte/eb/2022/09/27/neymar-comemora-gol-marcado-pela-selecao-brasileira-contra-a-tunisia-1664308063053_v2_450x600.jpg',
         name: 'Neymar Junior',
         platform: 'Instagram',
         country: 'BR',
-        followers: 210000000,
+        followers: '210000000',
+        category: "Esporte"
       },
     ], {});
   },
