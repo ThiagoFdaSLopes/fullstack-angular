00bd165b18fdcc59e49dfede87a72786972ef21e
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 17:58:12 -0300

    feat: SearchInfluencer By query

--- backend/src/controllers/Influencer.ts
+++ backend/src/controllers/Influencer.ts
@@ -9,4 +9,10 @@ export default class InfluencerController {
         if(!result) return res.status(500).json({ message: "Servico indisponivel"})
         res.status(200).json(result)
     }
+
+    public async SearchInfluencer(req: Request, res: Response): Promise<Response | void> {
+        const result = await this.influencerService.SearchInfluencer(req.query);
+        if(!result) return res.status(404).json({ message: "Influencers Nao Encontrado"})
+        res.status(200).json(result)
+    }
 }
