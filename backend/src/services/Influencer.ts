1e3cde8644c70b096c91ae64541ce911b2f4a908
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 17:59:28 -0300

    feat: SearchInfluencer Service

--- backend/src/services/Influencer.ts
+++ backend/src/services/Influencer.ts
@@ -1,5 +1,6 @@
-import { ModelStatic } from 'sequelize';
+import { ModelStatic, Op } from 'sequelize';
 import Influencer from '../database/models/Influencer';
+import IQuery from '../interfaces/IQuery';
 
 export default class InfluencerService {
     protected model: ModelStatic<Influencer> = Influencer;
@@ -7,5 +8,19 @@ export default class InfluencerService {
     async GetAllInfluencers(): Promise<Influencer[] | null> {
         const result = await this.model.findAll();
         return result;
+    }
+
+    async SearchInfluencer(query: IQuery): Promise<Influencer[] | null> {
+        const result = await this.model.findAll({
+            where: {
+                [Op.or]: [
+                    { name: { [Op.like]: `%${query.name}%`}},
+                    { followers: { [Op.like]: `%${query.followers}%`}},
+                    { country: { [Op.like]: `%${query.country}%`}},
+                    { category: { [Op.like]: `%${query.category}%`}},
+                ]
+            }
+        })
+        return result.length > 0 ? result : null;
     }
 }
