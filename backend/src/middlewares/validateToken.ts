47596ac95b866915b21096d82c5c8211ec66ccd2
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 12:23:05 -0300

    feat: Middleware ValidateToken

---
+++ backend/src/middlewares/validateToken.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { decodeToken } from '../utils/jwtToken';
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const decoded = decodeToken(authorization);
+    res.locals.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token must be a valid token' });
+  }
+};
+
+export default validateToken;
