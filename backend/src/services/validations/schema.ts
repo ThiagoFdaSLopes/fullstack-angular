03cd33272ae9a4b5fb366c2d5e17225a43a7dbc9
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 15:04:27 -0300

    feat: validate User Create

--- backend/src/services/validations/schema.ts
+++ backend/src/services/validations/schema.ts
@@ -1,9 +1,29 @@
 import Joi from 'joi';
 import { IUser } from '../../interfaces/IUser';
+import { IUserCreate } from '../../interfaces/IUserCreate';
 
 const errorMessage = 'All fields must be filled';
 
-const validateUserLogin = (body: IUser) => {
+export const validateUserLogin = (body: IUser) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.empty': errorMessage,
+      'string.email': 'Invalid email',
+      'string.required': errorMessage,
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid password length',
+      'string.required': errorMessage,
+    }),
+  });
+  const { error } = schema.validate(body);
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const validateUserCreate = (body: IUserCreate) => {
   const schema = Joi.object({
     email: Joi.string().email().required().messages({
       'string.empty': errorMessage,
@@ -15,11 +35,14 @@ const validateUserLogin = (body: IUser) => {
       'string.min': 'Invalid email or password',
       'string.required': errorMessage,
     }),
+    userName: Joi.string().min(3).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid userName length',
+      'string.required': errorMessage,
+    }),
   });
   const { error } = schema.validate(body);
   if (error) {
     throw new Error(error.message);
   }
-};
-
-export default validateUserLogin;
+}
