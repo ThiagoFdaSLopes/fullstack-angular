871e8c9a9a26cb42ae58a1d3fc948e1a13a3b80b
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 08:35:43 -0300

    fix: Resolvido problema para nao ter econrefused

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -5,7 +5,7 @@ const config: Options = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'db',
   port: Number(process.env.DB_PORT),
   dialect: 'mysql',
   dialectOptions: {
