75a2f5863d900e272d600c3225600628a67d52b1
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-11 12:23:28 -0300

    feat: Routes User

---
+++ backend/src/routes/User.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from 'express';
+import { UserController } from '../controllers';
+import validateToken from '../middlewares/validateToken';
+
+const userController = new UserController();
+
+const userRouter = Router();
+
+userRouter.post('/', (req: Request, res: Response) => userController.UserLogin(req, res));
+userRouter.get(
+  '/role',
+  validateToken,
+  (req: Request, res: Response) => res.status(200).json({ role: res.locals.user.role }),
+);
+
+export default userRouter;
