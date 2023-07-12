a9d6e94c2d4b12b2eeac71e925bd6aed7fb2be0f
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-12 09:02:09 -0300

    feat: Query Followers

--- backend/src/services/Influencer.ts
+++ backend/src/services/Influencer.ts
@@ -1,6 +1,7 @@
 import { ModelStatic, Op } from 'sequelize';
 import Influencer from '../database/models/Influencer';
 import IQuery from '../interfaces/IQuery';
+import IWhereClause from '../interfaces/IWhereClause';
 
 export default class InfluencerService {
     protected model: ModelStatic<Influencer> = Influencer;
@@ -11,15 +12,18 @@ export default class InfluencerService {
     }
 
     async SearchInfluencer(query: IQuery): Promise<Influencer[] | null> {
+        let whereClause: IWhereClause = {
+            [Op.or]: [
+                { name: { [Op.like]: `%${query.name}%`}},
+                { country: { [Op.like]: `%${query.country}%`}},
+                { category: { [Op.like]: `%${query.category}%`}},
+            ]
+        }
+        if (query.followers !== undefined) {
+            whereClause[Op.or].push({ followers: { [Op.gte]: Number(query.followers) } })
+        }
         const result = await this.model.findAll({
-            where: {
-                [Op.or]: [
-                    { name: { [Op.like]: `%${query.name}%`}},
-                    { followers: { [Op.like]: `%${query.followers}%`}},
-                    { country: { [Op.like]: `%${query.country}%`}},
-                    { category: { [Op.like]: `%${query.category}%`}},
-                ]
-            }
+            where: whereClause
         })
         return result.length > 0 ? result : null;
     }
