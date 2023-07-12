2bb61d54aa3bbe45a8a44d83bfa7637142aee23f
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-12 09:01:04 -0300

    fix: String para Integer

--- backend/src/database/migrations/20230711191515-influencers.js
+++ backend/src/database/migrations/20230711191515-influencers.js
@@ -25,7 +25,7 @@ module.exports = {
         allowNull: false,
       },
       followers: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
       category: {
