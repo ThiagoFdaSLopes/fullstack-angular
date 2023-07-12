a3f579e54cde799f1b494ac0e6d61e8e5905af34
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-12 18:16:15 -0300

    feat: Script para Instalar dependencias

---
+++ install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Função para instalar as dependências
+install_dependencies() {
+    folder=$1
+    cacheFolder=$2
+    
+    printf "\n> Entrando na pasta ${folder}\n"
+    cd ${folder}
+    
+    printf "\n> Executando npm install\n"
+    npm_config_loglevel=silent npm install --cache ${cacheFolder}
+    
+    printf "\n> Voltando para a pasta anterior\n"
+    cd -
+}
+
+# Instalar dependências do front-end
+install_dependencies "./frontend"
+
+# Instalar dependências do back-end
+install_dependencies "./backend"
