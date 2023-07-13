8f4d411b506870257e2a898064887912e9d54c8b
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-13 08:29:19 -0300

    feat: Schema Validator Influencer Creator

--- backend/src/services/validations/schema.ts
+++ backend/src/services/validations/schema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 import { IUser } from '../../interfaces/IUser';
 import { IUserCreate } from '../../interfaces/IUserCreate';
+import IInfluencer from '../../interfaces/IInfluencer';
 
 const errorMessage = 'All fields must be filled';
 
@@ -27,17 +28,56 @@ export const validateUserCreate = (body: IUserCreate) => {
   const schema = Joi.object({
     email: Joi.string().email().required().messages({
       'string.empty': errorMessage,
-      'string.email': 'Invalid email or password',
+      'string.email': 'Invalid email',
       'string.required': errorMessage,
     }),
     password: Joi.string().min(6).required().messages({
       'string.empty': errorMessage,
-      'string.min': 'Invalid email or password',
+      'string.min': 'Invalid password',
       'string.required': errorMessage,
     }),
     userName: Joi.string().min(3).required().messages({
       'string.empty': errorMessage,
       'string.min': 'Invalid userName length',
+      'string.required': errorMessage,
+    }),
+  });
+  const { error } = schema.validate(body);
+  if (error) {
+    throw new Error(error.message);
+  }
+}
+
+export const validateInfluencerCreate = (body: IInfluencer) => {
+  const schema = Joi.object({
+    image: Joi.string().required().messages({
+      'string.empty': errorMessage,
+      'string.email': 'Invalid image',
+      'string.required': errorMessage,
+    }),
+    name: Joi.string().min(3).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid name',
+      'string.required': errorMessage,
+    }),
+    platform: Joi.string().min(3).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid platform',
+      'string.required': errorMessage,
+    }),
+    country: Joi.string().min(2).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid country',
+      'string.required': errorMessage,
+    }),
+    followers: Joi.number().required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid followers',
+      'string.required': errorMessage,
+    }),
+    category: Joi.string().min(3).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid category',
       'string.required': errorMessage,
     }),
   });
