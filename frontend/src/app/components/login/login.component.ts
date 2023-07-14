caaa719ecd8c461dbac6b2d9ee186717c550da13
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-14 15:57:24 -0300

    feat: Register/Login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -14,8 +14,12 @@ export class LoginComponent {
   showMessageError: boolean = false
   inputEmailValidate: boolean = false
   inputPasswordValidate: boolean = false
+  inputUserNameValidate: boolean = false
+  disableButton: boolean = true
+  register: boolean = false
   email: string = ''
   password: string = ''
+  userName: string = ''
   messageError: string = ''
 
   constructor(private listService: ListService, private router: Router) {}
@@ -27,14 +31,20 @@ export class LoginComponent {
   handleInputChange(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     const value = inputElement.value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     
     if (inputElement.type === 'email') {
       this.email = value;
-      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
       !emailRegex.test(this.email) ? this.inputEmailValidate = !this.inputEmailValidate : this.inputEmailValidate = false;
-    } else {
-      this.password.length < 6 ? this.inputPasswordValidate = !this.inputPasswordValidate : this.inputPasswordValidate = false;
+    }
+
+    if(inputElement.type === 'password') {
       this.password = value;
+      this.password.length < 6 ? this.inputPasswordValidate = !this.inputPasswordValidate : this.inputPasswordValidate = false;
+    }
+    if(inputElement.type === 'text') {
+      this.userName = value;
+      this.userName.length < 3 ? this.inputUserNameValidate = !this.inputUserNameValidate : this.inputUserNameValidate = false;
     }
   }
 
@@ -43,6 +53,7 @@ export class LoginComponent {
     this.listService.Login({email: this.email, password: this.password}).subscribe(
       (response) => {
         localStorage.setItem("token", JSON.stringify(response.token))
+        this.listService.GetRole(response.token).subscribe((response) => localStorage.setItem("role", JSON.stringify(response.role)))
         this.show = !this.show
         this.showMessageError = false
         this.router.navigate(['/dashboard'])
@@ -53,5 +64,27 @@ export class LoginComponent {
         this.show = !this.show
       }
     );
+  }
+
+  Register() {
+    this.show = !this.show
+    this.listService.Register({ email: this.email, password: this.password, userName: this.userName}).subscribe(
+      (response) => {
+        localStorage.setItem("token", JSON.stringify(response.token))
+        this.listService.GetRole(response.token).subscribe((response) => localStorage.setItem("role", JSON.stringify(response.role)))
+        this.show = !this.show
+        this.showMessageError = false
+        this.router.navigate(['/dashboard'])
+      },
+      ({error}) => {
+        this.messageError = error.message
+        this.showMessageError = true
+        this.show = !this.show
+      }
+    );
+  }
+
+  ShowRegister() {
+    this.register = !this.register
   }
 }
