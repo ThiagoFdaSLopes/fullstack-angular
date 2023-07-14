05a0136c6870803aedec55c6f7b5cf89342b015e
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-14 12:45:22 -0300

    feat: SideBar Component

--- frontend/src/app/components/side-bar/side-bar.component.ts
+++ frontend/src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class SideBarComponent {
 
-}
+  dark: boolean = false
+  constructor(private renderer: Renderer2) {}
+
+  Darkmode() {
+    const body = document.body
+    this.dark = !this.dark
+    this.dark ? this.renderer.addClass(body, "dark") : this.renderer.removeClass(body, "dark")
+  }
+} 
