61093e66071c45b2a70128ec5e2296702cf19602
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-14 19:10:25 -0300

    feat: Interface Influencer

---
+++ frontend/src/app/interfaces/IInfluencer.ts
@@ -0,0 +1,9 @@
+export default interface IInfluencer {
+    id: number,
+    image: string,
+    name: string,
+    country: string,
+    followers: number,
+    platform: string,
+    category: string,
+}
