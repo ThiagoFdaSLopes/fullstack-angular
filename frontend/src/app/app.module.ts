c20f4119923b693b01f2b81f3ad13cad28e7eae4
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-14 12:32:24 -0300

    feat: Add Components and Http

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
+    LoginComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
