910accb5cea37a1ba2c74e8ead63801eb2cb8a77
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-15 14:19:49 -0300

    feat: Add Search Na tabela

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import IInfluencer from 'src/app/interfaces/IInfluencer';
+import ISearchInfluencer from 'src/app/interfaces/ISearchInfluencer';
 import { ListService } from 'src/app/services/list.service';
 
 @Component({
@@ -11,9 +12,19 @@ import { ListService } from 'src/app/services/list.service';
 })
 export class TableComponent implements OnInit   {
   influencers: IInfluencer[] = [];
+  searchFilter: ISearchInfluencer = {
+    name: '',
+    country: '',
+    category: '',
+    followers: 0,
+  }
+  isChecked: boolean = false
   permission: boolean = false;
   token = '';
   role = '';
+  showMessageError: boolean = false
+  errorMessage: string = ''
+  show: boolean = true
   constructor(private listService: ListService, private router: Router ) {}
 
   ngOnInit():void {
@@ -33,5 +44,25 @@ export class TableComponent implements OnInit   {
 
   Edit(id: number) {
     this.role === "admin" ? this.router.navigate([`edit/${id}`]) : alert("Usuario sem permissao");
+  }
+
+  Search() {
+    this.show = !this.show
+    this.listService.SearchInfluencer(this.token, this.searchFilter).subscribe(
+      (response) => {
+        this.influencers = response
+        this.showMessageError = false
+        this.show = !this.show
+      },
+      ({error}) => {
+        this.showMessageError = true
+        this.errorMessage = error.message
+        this.show = !this.show
+      }
+    )
+  }
+
+  RefreshPage(): void {
+    window.location.reload()
   }
 }
